5274e2d8f636a7506f533f6ac2112b1fffd43bac
gesroy <gesche.roy@gmail.com>
2018-09-03 16:03:11 +0200

    First functionality update score

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react'
 import './App.css'
+import Score from './Score.js'
+import Button from './Button.js'
+import ScoreUpdater from './ScoreUpdater'
 
 class App extends Component {
+  state = { score: 0 }
+
+  updateScore = score => {
+    this.setState({ score: score + this.state.score })
+  }
+
+  resetScore = () => {
+    this.setState({
+      score: 0,
+    })
+  }
+
   render() {
-    return <div className="App">Hello world</div>
+    return (
+      <div className="App">
+        <Score value={this.state.score} />
+        <ScoreUpdater onClick={this.updateScore} />
+        <Button text="Reset" onClick={() => this.setState({ score: 0 })}>
+          Reset
+        </Button>
+      </div>
+    )
   }
 }
 
