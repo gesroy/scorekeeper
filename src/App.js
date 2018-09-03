00f2fb7c7ea7894b86f7f9de8d6532bbaec9cbbd
gesroy <gesche.roy@gmail.com>
2018-09-03 12:31:08 +0200

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React, { Component } from 'react'
 import './App.css'
-import Header from './Header.js'
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <Header />
-      </div>
-    )
+    return <div className="App">Hello world</div>
   }
 }
 
